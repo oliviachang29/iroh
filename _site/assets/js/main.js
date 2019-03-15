6cba2a85379c879d54295eb6b262b386f83eff99
Olivia <cozymonster29@gmail.com>
2019-03-14 17:41:40 -0700

    keypress for new

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,11 +1,22 @@
 const sheety_link = "https://api.sheety.co/868c6085-df08-4d33-905d-c94a98bda3f6";
 var items;
 var scroll = new SmoothScroll('a[href*="#"]');
+var current_joke_index;
+
+function format(text) {
+	return text.replace(/\n/g, "<div class='spacer'></div>")
+}
 
 function getNew() {
-	var newItem = items[Math.floor(Math.random() * items.length)];
-	$('#text').text(newItem.quote);
-	$('#episode').text(newItem.episode);
+	var new_index = Math.floor(Math.random() * items.length);
+	while (new_index == current_joke_index) {
+		new_index = Math.floor(Math.random() * items.length);
+	}
+	var newItem = items[new_index];
+	$('#text').html(format(newItem.quote));
+	$('#episode').text(format(newItem.episode));
+	console.log(newItem.quote)
+	console.log(format(newItem.quote))
 	scroll.animateScroll(0);
 }
 
@@ -22,3 +33,13 @@ jQuery(window).on("load", function(){
 $('#reload').on("click",function(){
 	getNew()
 })
+
+$(document).keypress(function(e) {
+  getNew()
+});
+
+$("body").keydown(function(e) {
+  if(e.keyCode == 39) { // right
+    getNew()
+  }
+});
